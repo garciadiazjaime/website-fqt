fa4aa0f25b5e028efef50dcbc53f599d0d56bafd
David Blado <dblado@gmail.com>
2011-08-10 21:51:41 -0700

    first commit of Django example git repo w/ sqlite backend

--- setup.py
+++ setup.py
@@ -1,25 +1,10 @@
-#!/usr/bin/env python
-
 from setuptools import setup
 
-setup(
-    # GETTING-STARTED: set your app name:
-    name='website-fqt',
-    # GETTING-STARTED: set your app version:
-    version='1.0',
-    # GETTING-STARTED: set your app description:
-    description='FQT Website',
-    # GETTING-STARTED: set author name (your name):
-    author='Mint IT Media',
-    # GETTING-STARTED: set author email (your email):
-    author_email='info@mintitmedia.com',
-    # GETTING-STARTED: set author url (your url):
-    url='http://www.python.org/sigs/distutils-sig/',
-    # GETTING-STARTED: define required django version:
-    install_requires=[
-        'Django==1.4.6',
-    ],
-    dependency_links=[
-        'https://pypi.python.org/simple/django/'
-    ],
-)
+setup(name='YourAppName',
+      version='1.0',
+      description='OpenShift App',
+      author='Your Name',
+      author_email='example@example.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['Django>=1.3'],
+     )
