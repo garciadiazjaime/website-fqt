7207f46bdbf152bfcecf7ce40e00ffa148631934
Glen Baker <iepathos@gmail.com>
2013-04-13 18:17:44 -0700

    Fixed Django requirement causing Server Error 500
    The previous install requirement 'Django>=1.3' was installing Django 1.5.1, which resulted in an internal server error 500.  I adjusted to 'Django<=1.4', 1.4 was the most recent version I found immediately compatible when testing.

--- setup.py
+++ setup.py
@@ -3,23 +3,11 @@
 from setuptools import setup
 
 setup(
-    # GETTING-STARTED: set your app name:
-    name='website-fqt',
-    # GETTING-STARTED: set your app version:
+    name='YourAppName',
     version='1.0',
-    # GETTING-STARTED: set your app description:
-    description='FQT Website',
-    # GETTING-STARTED: set author name (your name):
-    author='Mint IT Media',
-    # GETTING-STARTED: set author email (your email):
-    author_email='info@mintitmedia.com',
-    # GETTING-STARTED: set author url (your url):
+    description='OpenShift App',
+    author='Your Name',
+    author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    # GETTING-STARTED: define required django version:
-    install_requires=[
-        'Django==1.4.6',
-    ],
-    dependency_links=[
-        'https://pypi.python.org/simple/django/'
-    ],
+    install_requires=['Django<=1.4'],
 )
