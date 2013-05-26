580ba557937caba2a7d053a7b2bbd56f24f09a0b
mint server <info@mintitmedia.com>
2013-05-26 15:06:05 -0500

    merge

--- fqt/settings.py
+++ fqt/settings.py
@@ -11,22 +11,18 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+     ('Mint IT Media', 'info@mintitmedia.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'fqt.sqlite',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-<<<<<<< HEAD
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'fqt',                      # Or path to database file if using sqlite3.
+        'USER': 'user_fqt',                      # Not used with sqlite3.
+        'PASSWORD': 'pssw_fqt',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-=======
-        'HOST': '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock',                      # Set to empty string for localhost. Not used with sqlite3.
->>>>>>> ad338c289df2e1aadd2299016af8d04b2d733919
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
