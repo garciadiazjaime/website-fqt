b54243585fa3cf585bfb482f48864238c7bdc89e
jaime <garciadiazjaime@gmail.com>
2017-03-18 09:02:08 -0500

    adjust to openshift

--- setup.py
+++ setup.py
@@ -4,20 +4,29 @@ from setuptools import setup
 
 setup(
     # GETTING-STARTED: set your app name:
-    name='YourAppName',
+    name='website-fqt',
     # GETTING-STARTED: set your app version:
     version='1.0',
     # GETTING-STARTED: set your app description:
-    description='OpenShift App',
+    description='FQT Website',
     # GETTING-STARTED: set author name (your name):
-    author='Your Name',
+    author='Mint IT Media',
     # GETTING-STARTED: set author email (your email):
-    author_email='example@example.com',
+    author_email='info@mintitmedia.com',
     # GETTING-STARTED: set author url (your url):
     url='http://www.python.org/sigs/distutils-sig/',
     # GETTING-STARTED: define required django version:
     install_requires=[
-        'Django==1.8.4'
+        'appdirs==1.4.3',
+        'Django==1.4.6',
+        'django-tinymce==2.0.5',
+        'gdata==2.0.18',
+        'packaging==16.8',
+        'pyparsing==2.2.0',
+        'python-slugify==1.1.4',
+        'six==1.10.0',
+        'South==1.0.2',
+        'Unidecode==0.4.18'
     ],
     dependency_links=[
         'https://pypi.python.org/simple/django/'
